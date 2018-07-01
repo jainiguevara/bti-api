890fea175ac707b6e1a7337afe41fc88dce201cb
Jaini Guevara <jaini.guevara@gmail.com>
2018-07-01 09:49:32 +0800

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,15 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    const user = this;
+    return user.update({
+        $pull : {
+            tokens: { token }
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function (token) {
     const User = this;
     let decoded;
